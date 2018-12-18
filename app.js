d100799f3ad4dc92987ac69eaed94739440835a5
CezarCobuz <cobuzcezar@gmail.com>
2018-12-18 23:37:03 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,73 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".scoreboard");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+function capitalizeFirstLetter (word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML =  userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = capitalizeFirstLetter(userChoice) + " beats " + capitalizeFirstLetter(computerChoice) + ". You win!";
+}
+
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = capitalizeFirstLetter(computerChoice) + " beats " + capitalizeFirstLetter(userChoice) + ". You lose!";
+}
+
+function draw(userChoice, computerChoice) {
+    result_p.innerHTML = "Draw!";
+}
+
+function getComputerChoice() {
+    const choices = ['rock','paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random()*3);
+    return choices[randomNumber];
+}
+
+function game(userChoice) {
+    const computerChoice= getComputerChoice();
+    switch (userChoice + " " + computerChoice) {
+        case "rock scissors":
+        case "paper rock":
+        case "scissors paper":
+            win(userChoice, computerChoice);
+            break;
+        case "rock paper":
+        case "paper scissors":
+        case "scissors rock":
+            lose(userChoice, computerChoice);
+            break;
+        case "rock rock":
+        case "paper paper":
+        case "scissors scissors":
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game('rock');
+    })
+
+    paper_div.addEventListener('click', function() {
+        game('paper');
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game('scissors');
+    })
+}
+
+main();
